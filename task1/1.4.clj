e8baf1fce2193e17dd605bbc5bb028b23ed99af7
tatyanakrivonogova <116897032+tatyanakrivonogova@users.noreply.github.com>
2025-09-27 15:02:33 +0700

    1.4.clj using recur

--- task1/1.4.clj
+++ task1/1.4.clj
@@ -1,14 +1,25 @@
 (ns task1.4
-(:gen-class))
+  (:gen-class))
 
-(defn create-strings [chars n]
-  (reduce (fn [strings i]
-            (reduce concat
-                    (map (fn [s] 
-                           (map (fn [c] (str s c))
-                                (filter (fn [c] (not= (last s) (first c))) chars)))
-                         strings)))
-          (list "")
-          (range n)))
-   
-(println (create-strings (list "a" "b" "c") 3))
+(defn extend-string [chars string]
+  (map #(str string %)
+       (filter #(not= (last string) (first %)) chars)))
+
+(defn extend-strings [strings chars]
+  (reduce concat
+        (map #(extend-string chars %) strings)))
+
+(defn create-strings 
+  ([chars n] (create-strings chars n chars))
+  ([chars n acc] 
+   (if (<= n 0) 
+       (list)
+       (if (= n 1)
+         acc
+         (recur chars
+                (dec n)
+                (extend-strings acc chars))))))
+
+(println (create-strings ["a" "b" "c"] 3))
+(println (create-strings (list "a" "b" "c") 5))
+(println (create-strings (list "a" "b") 10))
